2b8e290b42e458eff6c03eb74e9b3575500e28e5
nullFoo <38643612+nullFoo@users.noreply.github.com>
2022-01-24 18:39:22 +1300

    done

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # RedditVidDownloader
- title
+Does what it says in the title, downloads a video from a given reddit post
---
+++ main.py
@@ -0,0 +1,54 @@
+import os
+from moviepy.editor import *
+import praw
+import requests
+import urllib.request
+import glob
+import secrets
+
+reddit = praw.Reddit(client_id=secrets.clientID,
+                 client_secret=secrets.clientSecret,
+                 user_agent=secrets.prawName)
+      
+
+def logStr(string):
+    print("")
+    print("=================================")
+    print(string)
+    print("=================================")
+    print("")
+    
+def getVideo(link):
+    logStr("Finding media links")
+    r = requests.get(redditLink + ".json", headers = {'User-agent': secrets.clientID})
+    videoLink = r.json()[0]["data"]["children"][0]["data"]["secure_media"]["reddit_video"]["fallback_url"]
+    logStr("Getting video and audio data")
+    urllib.request.urlretrieve(
+        videoLink,
+        filename="tmp/video.mp4",
+    )
+    audioLink = videoLink.split("DASH_")[0] + "DASH_audio.mp4" + videoLink.split(".mp4")[1]
+    urllib.request.urlretrieve(
+        audioLink,
+        filename="tmp/audio.mp4",
+    )
+    logStr("Combining video and audio")
+    clip = VideoFileClip("tmp/video.mp4")
+
+    h = AudioFileClip("tmp/audio.mp4")
+    h.write_audiofile("tmp/video.mp3")
+    audioclip = AudioFileClip("tmp/video.mp3")
+
+    videoclip = clip.set_audio(audioclip)
+
+    videoclip.write_videofile("output/output.mp4")
+    
+def clearTempFiles():
+    logStr("Clearing temporary files")
+    files = glob.glob("tmp/*")
+    for f in files:
+        os.remove(f)
+
+redditLink = input("Post link: ")
+getVideo(redditLink)
+clearTempFiles()
